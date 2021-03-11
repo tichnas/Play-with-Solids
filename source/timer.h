261ceebb97dd7367ae075e1573095b3c99626050
tichnas <sanchit.arora.2002@gmail.com>
2021-03-11 16:29:53 +0530

    Initial commit

---
+++ source/timer.h
@@ -0,0 +1,15 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+class Timer {
+ public:
+  Timer() { prev = 0; }
+  Timer(double interval);
+  double interval;
+  bool processTick();
+
+ private:
+  double prev;
+};
+
+#endif  // TIMER_H
